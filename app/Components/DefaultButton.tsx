f41a53787e8ae54f6446a133b718108fe8609f92
Anna <annarosewain9@gmail.com>
2024-11-04 10:35:22 +0000

    Responsive styling for button

--- app/Components/DefaultButton.tsx
+++ app/Components/DefaultButton.tsx
@@ -8,7 +8,7 @@ const DefaultButton: React.FC<ButtonProps> = ({ text, onClick, className }) => {
   return (
     <button
       onClick={onClick}
-      className={`flex px-13 py-3 bg-none text-black rounded-lg ${className}`}
+      className={`m-5 px-10 py-3 uppercase font-medium text-[20px] tracking-wide bg-none text-black border-2 border-black rounded-lg hover:bg-black transition-colors duration-700 ease-in-out hover:text-[#CABB9D] ${className}`}
     >
       {text}
     </button>
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import DefaultButton from "@/app/Components/DefaultButton";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="uppercase flex justify-center items-center text-[28px] lg:text-[48px] leading-2 text-center pt-12 pb-3">
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="uppercase flex justify-center items-center text-[28px] lg:text-[48px] leading-2 text-center pt-10 pb-3">
         Breath - Mind - Body
       </h1>
       <HeroImage />
