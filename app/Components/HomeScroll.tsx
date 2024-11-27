65a7e0d3810a544c48f6fac418fcf264ed0ad326
Anna <annarosewain9@gmail.com>
2024-11-27 18:02:42 +0000

    Edited layout on HomeScroll

--- app/Components/HomeScroll.tsx
+++ app/Components/HomeScroll.tsx
@@ -6,6 +6,7 @@ import AnahataChakra from "@/public/Assets/courage-white.svg";
 import AjnaChakra from "@/public/Assets/clarity-white.svg";
 import MuladharaChakra from "@/public/Assets/presence-white.svg";
 import NextArrow from "@/public/Assets/arrow_forward.svg";
+import { ChevronRight } from "lucide-react";
 
 export default function HomeScroll() {
   {
@@ -35,33 +36,68 @@ export default function HomeScroll() {
   }
 
   return (
-    <div className="flex flex-col gap-y-[56px] lg:gap-y-[80px] lg:py-[120px] py-[56px]">
-      <div className="flex flex-col gap-x-6 sm:flex-row-reverse">
+    <div className="flex flex-col gap-y-[56px] lg:gap-y-[80px] lg:gap-y-[80px]lg:py-[120px] py-[56px]">
+      <div className="flex flex-col gap-x-12 items-center sm:flex-row-reverse w-full max-w-screen-lg mx-auto justify-center">
         <Image
           src={AnahataChakra}
           alt="Anatata Chakra - heart and courage"
-          className="hidden w-1/2 sm:block"
+          className="hidden w-2/5 sm:block"
         />
-        <div className="sm:w-1/2 lg:py-[56px] lg:pr-56px">
-          <h1 className="pt-[12px] text-2xl font-medium text-[#172026] lg:text-[42px] lg:leading-[58px]">
+        <div className="sm:w-1/2 lg:py-[56px] sm:px-10">
+          <h3 className="text-[1rem] text-customBeige">Anahata</h3>
+          <h1 className="pt-[12px] text-2xl font-medium text-white lg:text-[42px] lg:leading-[58px]">
             Build Courage
           </h1>
-          <Image
-            src={AnahataChakra}
-            alt="Sales Monitoring"
-            className="pt-[24px] sm:hidden"
-          />
-          <p className="py-[24px] text-[#36485C] lg:text-[18px]">
+          <div className="block sm:hidden lg:w-[600px] lg:h-[600px] mx-auto">
+            <Image
+              src={AnahataChakra}
+              alt="Anahata Chakra - heart and courage"
+            />
+          </div>
+          <p className="py-[24px] text-white lg:text-[18px]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
             ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip ex ea commodo consequat. 
           </p>
           <Link href="/work">
-            <p className="flex items-center gap-x-2 pt-6 font-medium text-[#0085FF] lg:text-[18px]">
+            <p className="flex items-center gap-x-2 pt-6 font-medium text-customGreen lg:text-[18px] ">
               Learn More{" "}
               <span>
-                <Image src={NextArrow} alt="Learn More" />
+                <ChevronRight />
+              </span>
+            </p>
+          </Link>
+        </div>
+      </div>
+      <div className="flex flex-col gap-x-6 sm:flex-row">
+        <Image
+          src={AnahataChakra}
+          alt="Anatata Chakra - heart and courage"
+          className="hidden w-2/5 sm:block"
+        />
+        <div className="sm:w-1/2 lg:py-[56px] sm:px-10">
+          <h3 className="text-[1rem] text-customBeige">Anahata</h3>
+          <h1 className="pt-[12px] text-2xl font-medium text-white lg:text-[42px] lg:leading-[58px]">
+            Build Courage
+          </h1>
+          <div className="block sm:hidden lg:w-[600px] lg:h-[600px] mx-auto">
+            <Image
+              src={AnahataChakra}
+              alt="Anahata Chakra - heart and courage"
+            />
+          </div>
+          <p className="py-[24px] text-white lg:text-[18px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. 
+          </p>
+          <Link href="/work">
+            <p className="flex items-center gap-x-2 pt-6 font-medium text-customGreen lg:text-[18px] ">
+              Learn More{" "}
+              <span>
+                <ChevronRight />
               </span>
             </p>
           </Link>
@@ -89,10 +125,7 @@ export default function HomeScroll() {
           </p>
           <Link href="/work">
             <p className="flex items-center gap-x-2 pt-6 font-medium text-[#0085FF] lg:text-[18px]">
-              Learn More{" "}
-              <span>
-                <Image src={NextArrow} alt="Learn More" />
-              </span>
+              Learn More <span></span>
             </p>
           </Link>
         </div>
@@ -120,16 +153,6 @@ export default function HomeScroll() {
             game-changing feature. Get a birds eye view with our customizable
             dashboard.
           </p>
-          <div className="flex w-full gap-x-[24px]">
-            <div className="w-1/2 flex flex-col ">
-              <h3 className="text-[20px] font-medium text-[#172026]">100+</h3>
-              <p className="text-[#36485C]">Lorem ipsum dolor si</p>
-            </div>
-            <div className="w-1/2 flex flex-col ">
-              <h3 className="text-[20px] font-medium text-[#172026]">100+</h3>
-              <p className="text-[#36485C]">Lorem ipsum dolor si</p>
-            </div>
-          </div>
           <Link href="/work">
             <p className="flex items-center gap-x-2 pt-6 font-medium text-[#0085FF] lg:text-[18px]">
               Learn More{" "}
