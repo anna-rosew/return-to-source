c1f73d6d9db79c300f54ebd87f86ab4a3f2de2dd
Anna <annarosewain9@gmail.com>
2024-12-08 19:32:56 +0000

    Started creating responsive NavBar

--- app/Components/NavBar.tsx
+++ app/Components/NavBar.tsx
@@ -4,12 +4,19 @@ import Image from "next/image";
 import Logo from "../../public/assets/Logo.svg";
 import User from "../../public/assets/User.svg";
 import Menu from "../../public/assets/Menu.svg";
-import NavBarLinks from "./NavBarLinks";
+import { Button } from "./ui/button";
 
 export function Navbar() {
+  const navLinks = [
+    { name: "Home", link: "/" },
+    { name: "Work with Me", link: "/work-with-me" },
+    { name: "About", link: "/about" },
+    { name: "Contact", link: "/contact" },
+    { name: "Blog", link: "/blog" },
+  ];
   return (
     <div className="flex w-full items-center justify-between px-5 py-2 border-b-[1px] border-black lg:container lg:mx-auto lg:px-5">
-      <div className="flex items-center">
+      <div className="flex items-center justify-between">
         <Link href="/">
           <Image
             src={Logo}
@@ -18,20 +25,24 @@ export function Navbar() {
             className="cursor-pointer"
           />
         </Link>
-        <NavBarLinks />
+        <ul className="md:flex md:items-center px-10 gap-x-[48px]">
+          {navLinks.map((item, index) => (
+            <li key={index} className="md:ml-8 text-xl">
+              <a
+                href={item.link}
+                className="font-jost text-[16px] text-black uppercase relative hover:text-black cursor-pointer transition-all ease-in-out inline-block before:transition-all before:ease-in-out before:duration-700 before:absolute before:bg-black before:origin-center before:h-[1px] before:w-0 hover:before:w-full hover:before:left-0 before:bottom-0"
+              >
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="flex gap-x-5 items-center">
-        <Link
-          href="/register"
-          className="hidden lg:block  opacity-75 text-[16px] pr-[20px]"
-        >
-          Open an Account
-        </Link>
-
-        <Link href="/login" className="hidden items-center gap-x-2 lg:flex">
+        <Button href="/login" variant="secondary">
           <Image src={User} alt="User Profile" width={38} />
-          <span className=" text-[16px]">Sign in</span>
-        </Link>
+          Sign in
+        </Button>
 
         <Image src={Menu} alt="Menu Button" className="lg:hidden" />
       </div>
