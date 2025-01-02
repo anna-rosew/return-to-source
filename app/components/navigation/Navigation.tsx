4b3b00b14afe7b5844a1feff007a89569ab2d333
Anna <annarosewain9@gmail.com>
2025-01-02 15:10:21 +0000

    Editing styling of new navbar

--- app/components/navigation/Navigation.tsx
+++ app/components/navigation/Navigation.tsx
@@ -24,33 +24,21 @@ const Navbar = () => {
   };
 
   return (
-    <nav>
+    <nav className="relative ">
       <div className="flex flex-wrap items-center justify-between max-w-screen-xl mx-auto p-4">
-        <Logo />
-        <div className="flex items-center md:order-2 space-x-1 md:space-x-2 rtl:space-x-reverse">
-          <Link
-            href="#"
-            className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-          >
-            Login
-          </Link>
-          <Link
-            href="#"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-          >
-            Sign up
-          </Link>
+        {/* Logo */}
+        <div className="flex items-center justify-between w-full md:w-auto">
+          <Logo />
           <button
             onClick={toggleMenu}
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 hover:cursor-pointer"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
             aria-controls="mega-menu"
             aria-expanded={menuOpen ? "true" : "false"}
           >
             <span className="sr-only">Open main menu</span>
             <svg
               className="w-5 h-5"
-              aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 17 14"
@@ -65,34 +53,51 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
+
+        {/* Login and Sign-up Buttons */}
+        <div className="flex items-center justify-center space-x-2 md:order-2">
+          <Link
+            href="#"
+            className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:hover:bg-gray-700 focus:outline-none"
+          >
+            Login
+          </Link>
+          <Link
+            href="#"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 md:px-5 md:py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none"
+          >
+            Sign up
+          </Link>
+        </div>
+
+        {/* Hamburger Menu */}
         <div
           id="mega-menu"
           className={`${
-            menuOpen ? "block" : "hidden"
+            menuOpen ? "block bg-customTeal" : "hidden"
           } items-center justify-between w-full md:flex md:w-auto md:order-1`}
         >
           <ul className="flex flex-col mt-4 md:flex-row md:mt-0 md:space-x-8 rtl:space-x-reverse">
-            <li>
-              <Link
-                href="#"
-                className="font-jost text-[16px] text-black uppercase relative hover:text-black cursor-pointer transition-all ease-in-out inline-block before:transition-all before:ease-in-out before:duration-700 before:absolute before:bg-black before:origin-center before:h-[1px] before:w-0 hover:before:w-full hover:before:left-0 before:bottom-0"
-                aria-current="page"
-              >
-                Home
+            {/* Home Link */}
+            <li className="py-2 px-3">
+              <Link href="#" aria-current="page">
+                <p className="font-jost text-[16px] text-black uppercase relative hover:text-black cursor-pointer transition-all ease-in-out inline-block before:transition-all before:ease-in-out before:duration-700 before:absolute before:bg-black before:origin-center before:h-[1px] before:w-0 hover:before:w-full hover:before:left-0 before:bottom-0">
+                  Home
+                </p>
               </Link>
             </li>
+
             {/* Work with Rose Dropdown */}
             <li>
               <button
                 onClick={() => toggleDropdown("workWithRose")}
-                className="flex items-center justify-between w-full py-2 px-3"
+                className="flex items-center justify-between w-full py-2 px-3 cursor-pointer"
               >
                 <p className="font-jost text-[16px] text-black uppercase relative hover:text-black cursor-pointer transition-all ease-in-out inline-block before:transition-all before:ease-in-out before:duration-700 before:absolute before:bg-black before:origin-center before:h-[1px] before:w-0 hover:before:w-full hover:before:left-0 before:bottom-0">
                   Work with Rose
                 </p>
                 <svg
-                  className="w-2.5 h-2.5 ms-3"
-                  aria-hidden="true"
+                  className="w-2.5 h-2.5 ms-3 cursor-pointer"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 10 6"
@@ -113,32 +118,18 @@ const Navbar = () => {
               >
                 <ul className="space-y-4 p-6 pb-0 text-gray-900 md:pb-4 dark:text-white">
                   <li>
-                    <Link
-                      href="#"
-                      className="text-customGreen dark:text-gray-400 hover:text-white dark:hover:text-blue-500"
-                    >
-                      Online Groups
-                    </Link>
+                    <Link href="#">Online Groups</Link>
                   </li>
                   <li>
-                    <Link
-                      href="#"
-                      className="text-customGreen dark:text-gray-400 hover:text-white dark:hover:text-blue-500"
-                    >
-                      In-Person Groups
-                    </Link>
+                    <Link href="#">In-Person Groups</Link>
                   </li>
                   <li>
-                    <Link
-                      href="#"
-                      className="text-customGreen dark:text-gray-400 hover:text-white dark:hover:text-blue-500"
-                    >
-                      Retreats
-                    </Link>
+                    <Link href="#">Retreats</Link>
                   </li>
                 </ul>
               </div>
             </li>
+
             {/* About Dropdown */}
             <li>
               <button
@@ -150,7 +141,6 @@ const Navbar = () => {
                 </p>
                 <svg
                   className="w-2.5 h-2.5 ms-3"
-                  aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 10 6"
@@ -171,24 +161,15 @@ const Navbar = () => {
               >
                 <ul className="space-y-4 p-6 pb-0 text-gray-900 md:pb-4 dark:text-white">
                   <li>
-                    <Link
-                      href="#"
-                      className="text-customGreen dark:text-gray-400 hover:text-white dark:hover:text-blue-500"
-                    >
-                      Our Story
-                    </Link>
+                    <Link href="#">Our Story</Link>
                   </li>
                   <li>
-                    <Link
-                      href="#"
-                      className="text-customGreen dark:text-gray-400 hover:text-white dark:hover:text-blue-500"
-                    >
-                      Mission
-                    </Link>
+                    <Link href="#">Mission</Link>
                   </li>
                 </ul>
               </div>
             </li>
+
             {/* Resources Dropdown */}
             <li>
               <button
@@ -200,7 +181,6 @@ const Navbar = () => {
                 </p>
                 <svg
                   className="w-2.5 h-2.5 ms-3"
-                  aria-hidden="true"
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 10 6"
@@ -221,20 +201,10 @@ const Navbar = () => {
               >
                 <ul className="space-y-4 p-6 pb-0 text-gray-900 md:pb-4 dark:text-white">
                   <li>
-                    <Link
-                      href="#"
-                      className="text-customGreen dark:text-gray-400 hover:text-white dark:hover:text-blue-500"
-                    >
-                      Blog
-                    </Link>
+                    <Link href="#">Blog</Link>
                   </li>
                   <li>
-                    <Link
-                      href="#"
-                      className="text-customGreen dark:text-gray-400 hover:text-white dark:hover:text-blue-500"
-                    >
-                      Tutorials
-                    </Link>
+                    <Link href="#">Tutorials</Link>
                   </li>
                 </ul>
               </div>
