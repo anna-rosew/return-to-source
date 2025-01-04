2e0eb216a9b4f0762731321e9a9818c1e4194229
Anna <annarosewain9@gmail.com>
2025-01-04 16:40:19 +0000

    Uploaded work images and started building cards

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import Approach from "@/public/Assets/Images/my-approach.webp";
+
 const Work = () => {
   return (
     <div className="text-center">
@@ -6,6 +10,22 @@ const Work = () => {
         Come and explore how your body, mind and breath work together. Open
         yourself to new dimensions of experience and personal growth.
       </p>
+      <div className="grid-cols-1 lg:grid-cols-3">
+        {" "}
+        <Link href="/approach" className="card-link">
+          <h2>Approach</h2>
+          <h4>LEARN FROM EXPERIENCE</h4>
+          <div className="work-image-container">
+            <Image
+              src={Approach}
+              alt="My Approach - 'Torus in acrylics': painting by Lynn Hanford-Day"
+              fill
+              className="rounded-lg object-cover"
+              placeholder="blur"
+            />
+          </div>
+        </Link>
+      </div>
     </div>
   );
 };
