f050eeb9cc141811e8bf443ba7589f26787a7c95
Anna <annarosewain9@gmail.com>
2025-01-10 11:49:56 +0000

    Removing unused env environment variable for now

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,4 @@
-import type { NextConfig } from "next";
-import config from "./config";
-
-const nextConfig: NextConfig = {
-  env: {
-    DB_URI: config.DB_URI,
-    API: config.API,
-    NEXTAUTH_SECRET: config.NEXTAUTH_SECRET,
-  },
-
+const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
@@ -19,6 +10,8 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  reactStrictMode: true,
 };
 
 export default nextConfig;
