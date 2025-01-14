2f34af1ee8f8d66844a631a52a8ff715fb4bbdad
Anna <annarosewain9@gmail.com>
2025-01-14 12:43:29 +0000

    Imported carousel component from ShadCN UI & created Testimonial Carousel

---
+++ components/ui/TestimonialCarousel.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+const TestimonialCarousel = () => {
+  return (
+    <div>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
+export default TestimonialCarousel;
