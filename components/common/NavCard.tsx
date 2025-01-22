bc16414c757a09a768c87ee3b12eb4beb789e514
Anna <annarosewain9@gmail.com>
2025-01-22 21:55:24 +0000

    Making NavCards narrower on mobile
    - Added responsive styling with min-h

--- components/common/NavCard.tsx
+++ components/common/NavCard.tsx
@@ -14,14 +14,13 @@ interface CardInfo {
 const NavCard = ({ cardInfo }: { cardInfo: CardInfo }) => {
   return (
     <div
-      className="rounded-lg group relative bg-cover bg-center transition-all duration-700 ease-in-out aspect-[1/1] min-h-[300px]"
+      className="rounded-lg group relative bg-cover bg-center transition-all duration-700 ease-in-out aspect-[1/1] max-h-[240px] md:min-h-[350px] w-full md:w-auto"
       style={{
         backgroundImage: `url(${cardInfo.imageUrl})`,
       }}
       aria-label={cardInfo.alt}
     >
       <Link href={cardInfo.link} className="card-link">
-        {/* Initial content */}
         <div className="rounded-lg absolute inset-0 flex items-center justify-center text-white bg-black bg-opacity-0 group-hover:bg-opacity-80 group-hover:bg-black transition-all duration-700">
           <div className="text-center">
             <h2 className="text-white mb-4">{cardInfo.title}</h2>
@@ -29,11 +28,9 @@ const NavCard = ({ cardInfo }: { cardInfo: CardInfo }) => {
           </div>
         </div>
 
-        {/* Hover content */}
         <div className="rounded-lg absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-700 bg-black">
           <div className="text-center text-white">
-            {/* Replace with your SVG */}
-            <SunIcon className=" hover:fill-white hover:text-white mx-auto" />
+            <SunIcon className=" hover:fill-white hover:text-white mx-auto my-4" />
             <p className="text-customBeige">{cardInfo.description}</p>
           </div>
         </div>
