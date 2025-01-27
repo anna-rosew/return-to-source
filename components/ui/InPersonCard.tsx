7031ae221b015826c1738f9b79dc60e3cc8a6f41
Anna <annarosewain9@gmail.com>
2025-01-27 23:12:45 +0000

    Fix button error
    - Added ghost variant

--- components/ui/InPersonCard.tsx
+++ components/ui/InPersonCard.tsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { ClockIcon } from "lucide-react";
+import React from "react";
 
 const InPersonCard = () => {
   return (
-    <div>
-      
+    <div
+      className="class-header p-4 flex flex-col items-start justify-start text-left md:items-center relative text-white bg-cover bg-center h-full rounded-lg sm:h-[300px] md:h-[500px] lg:h-[600px]"
+      style={{
+        backgroundImage: "url('/Assets/Images/about.webp')",
+      }}
+    >
+      <div className="flex flex-row items-center justify-start space-x-4">
+        <div
+          className="flex items-center justify-center bg-gray-100 bg-opacity-20 rounded-full p-3 text-white 
+        md:absolute md:top-4 md:left-4 md:flex-row md:items-start md:space-x-4"
+        ></div>
+        <div className="small-screen-component">
+          <p className="flex items-center bg-gray-100 bg-opacity-20 p-2 rounded-lg text-white">
+            <ClockIcon className="w-5 h-5 mr-2 text-white " />
+            30 mins
+          </p>
+        </div>
+      </div>
+      <h2 className="mt-2 md:mt-0 text-white small-screen-component">
+        Workshop
+      </h2>
     </div>
-  )
-}
+  );
+};
 
-export default InPersonCard
+export default InPersonCard;
--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -18,6 +18,7 @@ const buttonVariants = cva(
         "white-outline":
           "border border-white text-white hover:bg-white hover:text-white uppercase",
         link: "font-jost text-[16px] text-black uppercase relative hover:text-black cursor-pointer transition-all ease-in-out inline-block before:transition-all before:ease-in-out before:duration-700 before:absolute before:bg-black before:origin-center before:h-[1px] before:w-0 hover:before:w-full hover:before:left-0 before:bottom-0",
+        ghost: "bg-transparent text-black hover:bg-black hover:text-white",
       },
       size: {
         default: "h-10 px-4 py-2",
