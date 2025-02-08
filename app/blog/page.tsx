621f3fc244d66950aa6f1c658c49ccae395540c3
Anna <annarosewain9@gmail.com>
2025-02-08 15:26:56 +0000

    Blog page Build
    - Installed embla autoplay
    - Created a carousel for feature posts
    - Created a feature post component

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,52 @@
+"use client";
+import React from "react";
 import ConstructionLayout from "@/components/layout/ConstructionLayout";
+import Autoplay from "embla-carousel-autoplay";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 const Blog = () => {
+  const plugin = React.useRef(
+    Autoplay({ delay: 3000, stopOnInteraction: true })
+  );
   return (
     <ConstructionLayout>
       <div className="container text-center w-full">
-        <h1 className="text-center">Blog</h1>
+        <h1 className="pb-5">Blog</h1>
+        <p className="font-bold md:text-center">
+          Come and explore how your body, mind and breath work together. Open
+          yourself to new dimensions of experience and personal growth.
+        </p>
+        <Carousel
+          plugins={[plugin.current]}
+          className="w-full max-w-xs"
+          onMouseEnter={plugin.current.stop}
+          onMouseLeave={plugin.current.reset}
+        >
+          <CarouselContent>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <CarouselItem key={index}>
+                <div className="p-1">
+                  <Card>
+                    <CardContent className="flex aspect-square items-center justify-center p-6">
+                      <span className="text-4xl font-semibold">
+                        {index + 1}
+                      </span>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
       </div>
     </ConstructionLayout>
   );
