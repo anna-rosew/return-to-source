d535363f02c305418e05d5cf4398f910469ee356
Anna <annarosewain9@gmail.com>
2025-02-18 12:52:17 +0000

    FeaturedPosts Carsousel
    Switched to swiper for pagination and bullet options

--- components/ui/blog/FeaturedPosts.tsx
+++ components/ui/blog/FeaturedPosts.tsx
@@ -1,8 +1,11 @@
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "@/components/ui/carousel";
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
 import { Post } from "@/types";
 import { FeaturedSlide } from "./FeaturedSlide";
 
@@ -15,15 +18,30 @@ export function FeaturedPosts({ posts }: FeaturedPostsProps) {
 
   return (
     <section className="py-8">
-      <Carousel className="w-full max-w-6xl mx-auto">
-        <CarouselContent>
+      <div className="relative w-full max-w-6xl mx-auto">
+        <Swiper
+          modules={[Navigation, Pagination]}
+          slidesPerView={1}
+          navigation={{
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          }}
+          className="relative w-full aspect-[16/8]"
+        >
           {featuredPosts.map((post) => (
-            <CarouselItem key={post.slug} className="md:basis-1/3">
+            <SwiperSlide key={post.slug}>
               <FeaturedSlide post={post} />
-            </CarouselItem>
+            </SwiperSlide>
           ))}
-        </CarouselContent>
-      </Carousel>
+
+          <div className="medium-screen-component">
+            <div className="swiper-button-prev absolute left-4 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"></div>
+            <div className="medium-screen-component swiper-button-next absolute right-4 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer"></div>
+          </div>
+        </Swiper>
+      </div>
     </section>
   );
 }
+
+export default FeaturedPosts;
