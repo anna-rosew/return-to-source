0fb86ea1e42af7aca70b6a208d4645b41169947e
Anna <annarosewain9@gmail.com>
2025-02-22 12:36:40 +0000

    Data added to homescroll

--- content/serviceData.ts
+++ content/serviceData.ts
@@ -1,32 +1,32 @@
 import { IServiceCard } from "@/types";
-import AnahataChakra from "@/public/Assets/Images/HomepageBox1.webp";
-import AjnaChakra from "@/public/Assets/Images/HomepageBox3_.webp";
-import MuladharaChakra from "@/public/Assets/Images/HomepageBox3.webp";
+import HomeOne from "@/public/Assets/Images/HomepageBox1.webp";
+import HomeTwo from "@/public/Assets/Images/HomepageBox3_.webp";
+import HomeThree from "@/public/Assets/Images/HomepageBox3.webp";
 
 export const serviceData: IServiceCard[] = [
   {
-    serviceName: "Anahata",
-    title: "Build Courage",
+    serviceName: "Clarity",
+    title: "If your mind’s racing and you can’t switch off…",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    image: AnahataChakra,
+      "Learn how to regain clarity, and calm the turmoil within you.",
+    image: HomeOne,
     imageAlt: "Anatata Chakra - heart and courage",
     reversed: true,
   },
   {
-    serviceName: "Ajna",
-    title: "Find Clarity",
+    serviceName: "Restore",
+    title: "If your energy is drained and your body feels out of sync...",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    image: AjnaChakra,
+      "Discover how to reconnect with your body and the healing power within you.",
+    image: HomeTwo,
     imageAlt: "Ajna Chakra - intuition, imagination, self-realisation",
   },
   {
     serviceName: "Muladhara",
-    title: "Be Present",
+    title: "If you’re struggling to make sense of it all...",
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-    image: MuladharaChakra,
+      "Let’s work together so you can find the courage and clarity you need to navigate life, without adding to your stress.",
+    image: HomeThree,
     imageAlt: "Muladhara Chakra - Rooting and Grounding",
     reversed: true,
   },
